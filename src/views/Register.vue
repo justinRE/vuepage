c580b98f645c27a630b0aea0ae43362ea887b78b
JustinRE <justin@wesleyreisz.com>
2023-03-06 14:58:22 -0500

    Added verification

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,58 +6,63 @@
             <label for="username">First/Last Name</label>
             <input type="text" id="username" name="username" v-model="input.username" placeholder="First/Last Name" />
         </div>
+        <label>{{error1}}</label>
         <div class="form-inputs">
             <label for="password">Password</label>
             <input type="password" id="password" name="password" v-model="input.password" placeholder="Password" />
         </div>
+        <label>{{error2}}</label>
         <div class="form-inputs">
             <label for="phone">Phone Number</label>
             <input type="phone" id="phone" name="phone" v-model="input.phone" placeholder="Phone Number" />
         </div>
+        <label>{{error3}}</label>
         <div class="form-inputs">
             <label for="email">Email Address</label>
             <input type="email" id="email" name="email" v-model="input.email" placeholder="Email Address" />
         </div>
+        <label>{{error4}}</label> <br>
       </div>
       <button type="button" v-on:click="register()">Register</button>
       <div>
-        <label>{{message}}</label>
+        <label>{{message}}</label> 
       </div>
   </div>
 </template>
 
+
+
 <script>
-//probably want validation in another file
-const validateName = username => {
+export const validateName = username => {
   if (!username.length) {
     return { valid: false, error: "This field is required" };
   }
   return { valid: true, error: null };
 };
 
-const validatePhone = phone => {
+export const validatePhone = phone => {
   if (!phone.length) {
     return { valid: false, error: 'This field is required.' };
   }
 
-  if (!phone.match(/^[+][(]?[0-9]{1,3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,7}$/gm)) {
+  if (!phone.match(/^[+]?[(]?[0-9]{1,3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,7}$/gm)) {
     return { valid: false, error: 'Please, enter a valid international phone number.' };
   }
 
   return { valid: true, error: null };
 }
 
-const validateEmail = email => {
+export const validateEmail = email => {
   if (!email.length) {
     return { valid: false, error: "This field is required" };
   }
-  if (!email.match(/^\w+([.-]?\w+)_@\w+(_[_.-]?\w+)_(.\w{2,3})+$/)) {
+  if (!email.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*\.\w{2,3}$/)) {
     return { valid: false, error: "Please, enter a valid email." };
   }
   return { valid: true, error: null };
 };
 
-const validatePassword = password => {
+export const validatePassword = password => {
   if (!password.length) {
     return { valid: false, error: "This field is required" };
   }
@@ -66,8 +71,10 @@ const validatePassword = password => {
   }
   return { valid: true, error: null };
 };
+
+
     export default {
-        name: 'Register',
+        name: 'register', 
         data() {
             return {
                 input: {
@@ -76,60 +83,73 @@ const validatePassword = password => {
                     phone: "",
                     email: ""
                 },
-
+                valid: true,
+                success: false,
+                errors:{},
+                message: null,
+                error1:null,
+                error2:null,
+                error3:null,
+                error4:null,
+                users: []
             }
         },
+
         methods: {
             register() {
-              if(this.input.phone !== "" && this.input.email !== "") {
-                // create a user object with the input values
-                const user = {
-                  username: this.input.username,
-                  password: this.input.password,
-                  phone: this.input.phone,
-                  email: this.input.email
-                };
-                // add the user object to the array of registered users
-                this.users.push(user);
-                console.log("Registration successful");
-                message = "Registration successful";
-              }
-              else {
-                console.log("Phone number/email must be present");
-                message = "Phone number/email must be present"
+
+              this.errors= {};
+
+              const validName = validateName(this.input.username);
+              this.errors.username = validName.error;
+              if (!validName.valid) {
+                  this.valid = false;
               }
 
-              this.errors = {}
-
-              const validName = validateName(this.user.name);
-              this.errors.name = validName.error;
-              if (this.valid) {
-                this.valid = validName.valid
-              }
-
-              const validPhone = validatePhone(this.user.phone);
+              const validPhone = validatePhone(this.input.phone);
               this.errors.phone = validPhone.error;
               if (this.valid) {
                 this.valid = validPhone.valid
               }
 
-              const validEmail = validateEmail(this.user.email);
+              const validEmail = validateEmail(this.input.email);
               this.errors.email = validEmail.error;
               if (this.valid) {
                 this.valid = validEmail.valid
               }
 
-              const validPassword = validatePassword(this.user.password)
+              const validPassword = validatePassword(this.input.password)
               this.errors.password = validPassword.error
               if (this.valid) {
                 this.valid = validPassword.valid
               }
+            
 
               if (this.valid) {
-                alert('HURRAAYYY!! :-)\n\n' + JSON.stringify(this.user))
+                //do nothing if valid
               }
+
+            // Check if all fields are valid before registration
+            const validFields = Object.values(this.errors).every((error) => !error);
+            if (validFields) {
+              const user = {
+                name: this.input.username,
+                password: this.input.password,
+                phone: this.input.phone,
+                email: this.input.email,
+              };
+              this.users.push(user);
+              this.message = 'Registration successful';
+            } else {
+              this.message = 'Please fix the errors and try again.';  
+              this.error1 = validName.error;
+              this.error2 = validPassword.error;
+              this.error3 = validPhone.error;
+              this.error4 = validEmail.error;
             }
-        }
+
+          }
+      }
     }
 </script>
 
--- src/views/Secure.vue
+++ src/views/Secure.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="secure">
+  <div class="Punch card">
     <h1>Punch card</h1>
     <img :src="require('@/assets/coffeecard.jpeg')" />
   </div>
 </template>
+
+<validate-jwt header-name="Authorization" failed-validation-httpcode="401" failed-validation-error-message="Unauthorized. Access token is missing or invalid.">
+  <openid-config url="https://login.microsoftonline.com/{aad-tenant}/v2.0/.well-known/openid-configuration" />
+  <required-claims>
+      <claim name="aud">
+          <value>144775ae-a710-470c-b465-50fa7a3a227f</value>
+      </claim>
+  </required-claims>
+</validate-jwt>
 
 <script>
     export default {
