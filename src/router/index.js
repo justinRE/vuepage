cd8be3b2c0309f5f3e60aad60c0666ad04fb5fd0
JustinRE <justin@wesleyreisz.com>
2023-04-16 11:27:14 -0400

    Made vuex persist after page refresh

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ const oktaAuth= new OktaAuth({
   clientId: '0oa8xojnpxKG707955d7',
   issuer: 'https://dev-71617988.okta.com/oauth2/default',
   redirectUri: window.location.origin + '/login/callback',
+  scopes: ['openid', 'profile', 'email', 'phone']
 })
 
 Vue.use(Auth, { oktaAuth })
