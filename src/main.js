1cc4ecc0cb3d5bf41fabf0dc71a315cc945f7d26
JustinRE <justin@wesleyreisz.com>
2023-04-19 11:47:36 -0400

    Removed Okta from main.js

--- src/main.js
+++ src/main.js
@@ -4,14 +4,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import { OktaAuth } from '@okta/okta-auth-js'
 import OktaVue from '@okta/okta-vue'
-import config from '@/config'
 import store from './store';
-
-const oktaAuth = new OktaAuth(config.oidc)
-
-Vue.use(OktaVue, {
-  oktaAuth,
-})
 
 new Vue({
   router,
