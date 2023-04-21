35a2599d531a094bff28735fe5306d1cbb20009b
JustinRE <justin@wesleyreisz.com>
2023-04-21 11:36:19 -0400

    Added debug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,23 +4,17 @@
     At Collide, we invite everyone to be a part of the good. For us, that means supporting community empowerment in the countries where our coffee comes from. 
     
     We donate 10% of every purchase toward local-led healthcare programs in underdeveloped communities in the Dominican Republic. By supporting Collide, you’re providing affordable, Dominican-led health and dental care to women, men, and children who would not otherwise receive care.
-    {{ headerColor }}
-    {{ headerTitleColor }}
-    {{ VUE_APP_KEY }}
+    {{ KEY }}
   </div>
 </template>
 
 <script>
-
-export default{
+export default {
   name: 'Home',
-  data(){
-
-  },
-  mounted(){
-    const headerColor = process.env.HEADER_COLOR;
-    const headerTitleColor =  process.env.HEADER_TITLE_COLOR;
-    const VUE_APP_KEY = process.env.VUE_APP_KEY;
+  computed: {
+    KEY() {
+      return process.env.VUE_APP_KEY;
+    }
   }
 }
 </script>
