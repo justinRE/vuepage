1066c4acb6bf36bfe612fd546a7ceda322f04e68
JustinRE <justin@wesleyreisz.com>
2023-04-21 14:19:17 -0400

    Setting user info in Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
-        <h2>Welcome {{ $store.state.name }}</h2>
-        <div>{{ $store.state.email }}</div>
-        <div>{{ $store.state.phone }}</div>
-        <div>{{ $store.state.role }}</div>
-        <div>{{ info }}</div>
-    </div>
+  <div>
+      <h2>Welcome {{ $store.state.name }}</h2>
+      <div>{{ $store.state.email }}</div>
+      <div>{{ $store.state.phone }}</div>
+      <div>{{ $store.state.role }}</div>
+      <div>{{ info }}</div>
+  </div>
 </template>
 
 <script>
@@ -13,47 +13,46 @@ import { mapActions } from 'vuex'
 import axios from 'axios';
 
 export default {
-  name: 'Profile',
-  data () {
-    return {
-      claims: [],
-      info: null
-    }
-  },
-  mounted(){
-    axios
-      .get('/.auth/me')
-      .then(response => (this.info = response))
-  },
-  async created () {
-    const idToken = await this.$auth.tokenManager.get('idToken')
-    this.claims = await Object.entries(idToken.claims).map(entry => ({ claim: entry[0], value: entry[1] }))
-
-    var name = this.claims.find(claim => claim.claim === 'name').value
-    console.log("setting name: " + name)
-    this.setName(name)
-
-    var email = this.claims.find(claim => claim.claim === 'email').value
-    console.log("setting email: " + email)
-    this.setEmail(email)
-
-    var phone = "502-802-6596"
-    console.log("setting phone [mocked]: " + phone)
-    this.setPhone(phone)
-
-    var token = this.claims.find(claim => claim.claim === 'nonce').value
-    console.log("Setting token: " + token)
-    this.setToken(token)
-  },
-  methods: {
-    ...mapActions(['setName']),
-    ...mapActions(['setEmail']),
-    ...mapActions(['setPhone']),
-    ...mapActions(['setToken']),
-    GetUserInfo(){
-    }
-}
+name: 'Profile',
+data () {
+  return {
+    claims: [],
+    info: null
   }
+},
+mounted(){
+  axios
+    .get('/.auth/me')
+    .then(response => (this.info = response))
+},
+async created () {
+  const userDetails = this.info.data.clientPrincipal.userDetails;
+
+  var name = userDetails.split('@')[0];
+  console.log("setting name: " + name)
+  this.setName(name)
+
+  var email = userDetails;
+  console.log("setting email: " + email)
+  this.setEmail(email)
+
+  var phone = "502-802-6596"
+  console.log("setting phone [mocked]: " + phone)
+  this.setPhone(phone)
+
+  var roles = this.info.data.clientPrincipal.userRoles;
+  console.log("Setting roles: " + roles);
+  this.setRole(roles);
+},
+methods: {
+  ...mapActions(['setName']),
+  ...mapActions(['setEmail']),
+  ...mapActions(['setPhone']),
+  ...mapActions(['setRole']),
+  GetUserInfo(){
+  }
+}
+}
 </script>
 
 <style>
