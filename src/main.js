ba3e00613b03f4f3b742dd38342ece315c3f1049
JustinRE <justin@wesleyreisz.com>
2023-05-01 11:00:24 -0400

    Updated Customers again

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store';
+import store from './store'
+import ToastPlugin from 'vue-toast-notification';
 
 new Vue({
   router,
   store,
+  ToastPlugin,
   render: function (h) { return h(App) }
 }).$mount('#app')
